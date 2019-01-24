410c06be14f16a2ecc5fb284ae634715380d90b7
Phil Austin <paustin@eos.ubc.ca>
2019-01-24 13:03:46 -0800

    fixed

--- Lec5_Euler_debug.py
+++ Lec5_Euler_debug.py
@@ -43,7 +43,7 @@
 #       height: calc(100% - 180px)
 #       left: 10px
 #       top: 150px
-#       width: 298px
+#       width: 165px
 #     toc_section_display: true
 #     toc_window_display: true
 # ---
@@ -58,24 +58,25 @@
 # The choice of the timestep matters. Look at the previous equation:
 #
 # \begin{equation}
-#   \frac{\color{blue}{c_{\text{TMF}}}(t + \Delta t)-c_{\text{TMF}}(t)}{\Delta t} =  \frac{Q_{\text{pit}}  c_{\text{pit}} + Q_{\text{mill}}  
-#   c_{\text{mill}} + A_{\text{bottom}} k \left( c_{\text{pore}} -   {\color{red}{c_{\text{TMF}}}}  \right)  
-#   -Q_{\text{dis}} \color{red}{c_{\text{TMF}}}}{V_0}
+# \frac{\color{blue}{c_{\text{TMF}}}(t + \Delta t)-c_{\text{TMF}}(t)}{\Delta t} =  \frac{Q_{\text{pit}}  c_{\text{pit}} + Q_{\text{mill}}  
+# c_{\text{mill}} + A_{\text{bottom}} k \left( c_{\text{pore}} -   {\color{red}{c_{\text{TMF}}}}  \right)  
+# -Q_{\text{dis}} \color{red}{c_{\text{TMF}}}}{V_0}
 # \end{equation}
 #
-# To compute the solution $c_{\text{TMF}}(t+ \Delta t)$, we use the value of $c_{\text{TMF}}$. But, at what time? Intrinsically, at every time between $t$ and $ t + \Delta t$, the value of $c_{\text{TMF}}$ changes.
+# To compute the solution $c_{\text{TMF}}(t+ \Delta t)$, we use the value of $c_{\text{TMF}}$. But, at what time? Intrinsically, at every time between $t$ and $t + \Delta t$, the value of $c_{\text{TMF}}$ changes.
 #
 # So the timestep should be small!
 # %% [markdown]
 # \begin{equation}
 # \begin{array}{lll}
-#   c_\infty & = & \frac{Q_{\text{pit}}  c_{\text{pit}} + Q_{\text{mill}}  c_{\text{mill}} + kA c_{\text{pore}} }{Q_{\text{dis}}+kA} \\
-#   & = & \frac{\color{blue}{Q_{\text{pit}}  c_{\text{pit}}} + \color{blue}{Q_{\text{mill}}  c_{\text{mill}}} + kA c_{\text{pore}} }{\color{blue}{Q_{\text{dis}}}+kA} \\
-#   & \approx & \frac{kA c_{\text{pore}} }{kA} \\
-#   & \approx & c_{\text{pore}} = 2000 \text{ mg/L}
+# c_\infty & = & \frac{Q_{\text{pit}}  c_{\text{pit}} + Q_{\text{mill}}  c_{\text{mill}} + kA c_{\text{pore}} }{Q_{\text{dis}}+kA} \\
+# & = & \frac{\color{blue}{Q_{\text{pit}}  c_{\text{pit}}} + \color{blue}{Q_{\text{mill}}  c_{\text{mill}}} + kA c_{\text{pore}} }{\color{blue}{Q_{\text{dis}}}+kA} \\
+# & \approx & \frac{kA c_{\text{pore}} }{kA} \\
+# & \approx & c_{\text{pore}} = 2000 \text{ mg/L}
 # \end{array}
 # \end{equation}
 
 # %% {"lines_to_next_cell": 2}
+
 
 
