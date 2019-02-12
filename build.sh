aefe704b5165f516bf11fb132554fc6c8e302330
Phil Austin <paustin@eos.ubc.ca>
2019-02-12 11:03:46 -0800

    add log

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -v
 rm -rf _build/*
-sphinx-build -v -N -b html .  _build
+sphinx-build -vv -N -b html .  _build > build.log
 jupyter nbconvert --to html img_fig_eq.ipynb --stdout >  _build/img_fig_eq_nbconvert.html
 jupyter nbconvert --to html md_fig_eq_aligh.ipynb --stdout > _build/md_fig_eq_aligh_nbconvert.html
 cp -a figures _build/.
