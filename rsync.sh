0bbf000b9ddef0a81cf3aed633091f3b5dc31993
Phil Austin <paustin@eos.ubc.ca>
2019-02-12 14:07:03 -0800

    figures

--- rsync.sh
+++ rsync.sh
@@ -1,2 +1,4 @@
 #!/bin/bash -v
 rsync -avz _build/* kiteh:/home/phil/public_html/github/nbsphinx/.
+rsync -avz _build/*nbconvert*html kiteh:/home/phil/public_html/github/nbsphinx/nbconvert/.
+rsync -avz _build/figures kiteh:/home/phil/public_html/github/nbsphinx/nbconvert/
